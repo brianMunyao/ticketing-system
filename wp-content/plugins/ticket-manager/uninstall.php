a1da56596c799a44eb94453f762c854c1f6fdd55
brianMunyao <brianmunyao6@gmail.com>
2023-07-14 06:33:20 +0000

    Remove the now ignored directory

--- wp-content/plugins/ticket-manager/uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * @package TicketManager
- */
-
-!defined('WP_UNINSTALL_PLUGIN') or exit();
--- wp-content/themes/MyTheme/footer.php
+++
@@ -1,8 +0,0 @@
-<?php wp_footer(); ?>
-
-<footer>
-    &copy; 2023 Ticketing System. All rights reserved.
-</footer>
-</body>
-
-</html>
